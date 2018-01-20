df3f1ae5b47fcb24c36f39586ad50380bbf8051f
riceluxs1t <riceluxs1t@gmail.com>
2018-01-20 15:30:14 -0600

    representations added.

---
+++ models/data_representations.py
@@ -0,0 +1,30 @@
+
+
+class GetContributorRequest(object):
+    def __init__(self):
+        pass
+
+    def get_repo_id(self):
+        pass
+
+
+class GetContributorResponse(object):
+    def __init__(self):
+        pass
+
+    def get_contributors(self):
+        pass
+
+
+class Contributor(object):
+    def __init__(self):
+        pass
+
+    def get_num_commits(self):
+        pass
+
+    def get_num_lines_added(self):
+        pass
+
+    def get_num_lines_deleted(self):
+        pass
